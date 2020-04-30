7a13a758acd0bf1714cb41ddced72f16b6e7e19f
jashaikh-dev <junaidazharshaikh@gmail.com>
2020-04-29 21:10:25 -0400

    chores: forgot to save User

--- src/ts-models/Models/User.cs
+++ src/ts-models/Models/User.cs
@@ -2,6 +2,12 @@ namespace ts_models.Models
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int GenderId { get; set; }
+
+        public int AddressId { get; set; }
     }
 }
